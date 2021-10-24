89c468b13e3c23f3930f392d871f741dc14ba402
henryolazabal <henryolazabal@gmail.comm>
2021-10-24 10:23:55 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './CC/heroe/heroe.component';
+import { ListadoComponent } from './CC/listado/listado.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent ,
+    HeroeComponent,
+    ListadoComponent
+   
+  ],
+  imports: [
+    BrowserModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
